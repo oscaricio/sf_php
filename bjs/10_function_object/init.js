9239069a8951a6b2bf7b9f0fbb7fece3eab3d907
PS Evgeny <oscaricio@yandex.ru>
2023-05-20 14:46:36 +0300

    update tast_9.11

--- bjs/10_function_object/init.js
+++ bjs/10_function_object/init.js
@@ -1,7 +1,46 @@
+let firstNameNode = document.getElementById('firstNameOutput'),
+    surnameNode = document.getElementById('surnameOutput'),
+    lastNameNode = document.getElementById('lastNameOutput'),
+    genderNode = document.getElementById('genderOutput'),
+    professionNode = document.getElementById('professionOutput'),
+    birthDateNode = document.getElementById('birthDateOutput'),
+    firstNameDefaultText = firstNameNode.innerText,
+    surnameDefaultText = surnameNode.innerText,
+    lastNameDefaultText = lastNameNode.innerText,
+    genderDefaultText = genderNode.innerText,
+    professionDefaultText = professionNode.innerText,
+    birthDateDefaultText = birthDateNode.innerText;
+
+function init() {
+    const initPerson = personGenerator.getPerson();
+    firstNameNode.innerText = initPerson.firstName;
+    surnameNode.innerText = initPerson.surname;
+    lastNameNode.innerText = initPerson.lastName;
+    genderNode.innerText = initPerson.gender;
+    professionNode.innerText = initPerson.profession;
+    birthDateNode.innerText = initPerson.birthDate + ' ' + initPerson.birthMonth + ' ' + initPerson.birthYear + ' года рождения';
+}
+
+function clearPerson() {
+    firstNameNode.innerText = firstNameDefaultText;
+    surnameNode.innerText = surnameDefaultText;
+    lastNameNode.innerText = lastNameDefaultText;
+    genderNode.innerText = genderDefaultText;
+    professionNode.innerText = professionDefaultText;
+    birthDateNode.innerText = birthDateDefaultText;
+}
 
 window.onload = function()
 {
-    const initPerson = personGenerator.getPerson();
-    document.getElementById('firstNameOutput').innerText = initPerson.firstName;
+    init();
+
+    //const initPerson = personGenerator.getPerson();
+    // document.getElementById('firstNameOutput').innerText = initPerson.firstName;
+    // document.getElementById('surnameOutput').innerText = initPerson.surname;
+    // document.getElementById('lastNameOutput').innerText = initPerson.lastName;
+    // document.getElementById('genderOutput').innerText = initPerson.gender;
+    // document.getElementById('professionOutput').innerText = initPerson.profession;
+    //
+    // document.getElementById('birthDateOutput').innerText = initPerson.birthDate + ' ' + initPerson.birthMonth + ' ' + initPerson.birthYear + ' года рождения';
 };
 
--- bjs/10_function_object/personGenerator.js
+++ bjs/10_function_object/personGenerator.js
@@ -35,6 +35,65 @@ const personGenerator = {
             "id_10": "Андрей"
         }
     }`,
+    firstNameFemaleJson: `{
+        "count": 11,
+        "list": {     
+            "id_1": "Инна",
+            "id_2": "Александра",
+            "id_3": "Дарья",
+            "id_4": "Марина",
+            "id_5": "Татьяна",
+            "id_6": "Виктория",
+            "id_7": "Анна",
+            "id_8": "Екатерина",
+            "id_9": "Валентина",
+            "id_10": "Анастасия",
+            "id_11": "Ольга"
+        }
+    }`,
+    maleProfession: `{
+        "count": 8,
+        "list": {
+            "id_1": "слесарь",
+            "id_2": "шахтёр",
+            "id_3": "солдат",
+            "id_4": "водитель",
+            "id_5": "учитель",
+            "id_6": "писатель",
+            "id_7": "менеджер по туризму",
+            "id_8": "программист"
+        }
+    }`,
+    femaleProfession: `{
+        "count": 8,
+        "list": {
+            "id_1": "секретарь",
+            "id_2": "няня",
+            "id_3": "лифтёр",
+            "id_4": "вахтёр",
+            "id_5": "учитель",
+            "id_6": "писатель",
+            "id_7": "менеджер по туризму",
+            "id_8": "программист"
+        }
+    }`,
+    month: `{
+        "count": 12,
+        "list": {
+            "id_1": "января",
+            "id_2": "февраля",
+            "id_3": "марта",
+            "id_4": "апреля",
+            "id_5": "мая",
+            "id_6": "июня",
+            "id_7": "июля",
+            "id_8": "августа",
+            "id_9": "сентября",
+            "id_10": "октября",
+            "id_11": "ноября",
+            "id_12": "декабря"
+        }
+    }`,
 
     GENDER_MALE: 'Мужчина',
     GENDER_FEMALE: 'Женщина',
@@ -48,23 +107,88 @@ const personGenerator = {
     },
 
     randomFirstName: function() {
-
-        return this.randomValue(this.firstNameMaleJson);
-
+        return this.isMale ? this.randomValue(this.firstNameMaleJson) : this.randomValue(this.firstNameFemaleJson);
     },
 
 
      randomSurname: function() {
+        return this.isMale ? this.randomValue(this.surnameJson) : this.randomValue(this.surnameJson) + 'а';
+    },
 
-        return this.randomValue(this.surnameJson);
+    randomLastName: function() {
+        let maleName = this.randomValue(this.firstNameMaleJson),
+            isMale = this.isMale,
+            root = maleName;
 
+        /**
+         * Если последняя буква "[а,я]":
+         *     Если предпоследняя "т", то жен: "ична", муж: "ич" (Никита: Никитична, Никитич)
+         *     Если предпоследняя "!т", то жен: "инична", муж: "ич" (Илья: Ильинична, Ильич)
+         * Если последня буква "й", то жен: "евна" б муж: "евич" (Евгений: Евгениевна, Евгениевич; Андрей: Адреевна, Андреевич
+         * Остальные, жен: "овна", муж: "ович" (Иван: Ивановна, Иванович)
+         */
+
+        if (["а", "я"].indexOf(maleName.substr(maleName.length-1,1)) !== -1) {
+            root = maleName.substring(0, maleName.length-1);
+            if (isMale) {
+                return root += "ич";
+            } else {
+                if (maleName.substr(maleName.length-2,1) === "т") {
+                    return root += "ична";
+                } else {
+                    return root += "инична";
+                }
+            }
+        } else if (maleName.substr(maleName.length-1,1) === "й") {
+            root = maleName.substring(0, maleName.length-1);
+            if (isMale) {
+                return root += "евич";
+            } else {
+                return root += "евна";
+            }
+        } else {
+            if (isMale) {
+                return root += "ович";
+            } else {
+                return root += "овна";
+            }
+        }
+    },
+
+    randomProfession: function () {
+        return this.isMale ? this.randomValue(this.maleProfession) : this.randomValue(this.femaleProfession);
+    },
+
+    randomDate: function () {
+        let month = this.person.month,
+            minDate = 1,
+            maxDate = 31;
+
+        if (month === 'февраля') maxDate = 28;
+        else if (['апреля', 'июня', 'сентября', 'октября'].indexOf(month) !== -1) maxDate = 30;
+
+        return this.randomIntNumber(maxDate, minDate);
     },
 
 
+    randomGender: function () {
+        this.isMale = this.randomIntNumber(); //1 - Мужчина, 0 - Женщина
+        return this.isMale ? this.GENDER_MALE : this.GENDER_FEMALE;
+    },
+
     getPerson: function () {
         this.person = {};
-        // this.person.gender = this.randomGender();
+        this.person.gender = this.randomGender();
         this.person.firstName = this.randomFirstName();
+        this.person.surname = this.randomSurname();
+        this.person.lastName = this.randomLastName();
+        this.person.profession = this.randomProfession();
+
+        this.person.birthYear = this.randomIntNumber(2023, 1950);
+        this.person.birthMonth = this.randomValue(this.month);
+        this.person.birthDate = this.randomDate();
+
+        console.log(this.person);
         return this.person;
     }
 };
